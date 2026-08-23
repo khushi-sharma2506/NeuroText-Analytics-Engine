5f4665ba7a1c378170f3e8240a9222818449f295
Khushi Sharma <khushisharma0300825@gmail.com>
2026-08-23 20:45:12 +0530

    Optimize PyTorch memory footprint to prevent Render OOM crash

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
+import os
+# Force PyTorch and numpy to use a single thread to save massive amounts of RAM
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
+import torch
+torch.set_num_threads(1)
+
 from transformers import pipeline
 import spacy
 from pypdf import PdfReader
